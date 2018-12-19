f645ffe665592305e3c5e78975ca2c075036db10
Antonio Mesquita <antonio@parafernalia.net.br>
2018-12-19 15:11:33 -0200

    Feat: blog page finished

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -3,7 +3,14 @@ import _ from 'lodash';
 
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
     await dispatch(fetchPosts());
-    console.log(getState().posts);
+    //const userIds = _.uniq(_.map(getState().posts,'userId'));
+    //userIds.forEach(id => dispatch(fetchUser(id)));
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
 }
 export const fetchPosts =  () => async (dispatch) => {
     const response = await jsonPlaceHolder.get('/posts');
