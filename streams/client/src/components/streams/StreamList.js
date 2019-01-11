28144fe29d7179d7114f211f28159fe7deb3f4e5
antoniomesquita09 <antoniomesquita09@gmail.com>
2019-01-11 16:04:35 -0200

    Wip: stream delete and edit

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -6,13 +6,26 @@ class StreamList extends React.Component {
     componentDidMount(){
         this.props.fetchStreams();
     }
+
+    renderAdmin(stream){
+        if(stream.userId === this.props.currentUserId){
+            return (
+                <div className="right floated content">
+                    <button className="ui button primary">Edit</button>
+                    <button className="ui button negative">Delete</button>
+                </div>
+            );
+        }
+    }
+
     renderList(){
         return this.props.streams.map(stream => {
             return (
                 <div ClassName="item" key={stream.id}>
+                    {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
-                        <div className="header" >{stream.title}</div>
+                        {stream.title}
                         <div className="description">{stream.description}</div>
                     </div>
                 </div>
@@ -30,7 +43,10 @@ class StreamList extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-    return { streams: Object.values(state.streams) };
+    return {
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId
+    };
 }
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
