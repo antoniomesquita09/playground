d8fbaec918ff148101950712d20d834481e7b0b5
antoniomesquita09 <antoniomesquita09@gmail.com>
2019-01-29 15:53:05 -0200

    Wip: new stylesheet

--- hunt/src/pages/main.js
+++ hunt/src/pages/main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, ScrollView, Dimensions } from 'react-native';
 import api from '../services/api';
+import styles from './styles';
 
 export default class Main extends Component {
     static navigationOptions = {
@@ -14,7 +15,9 @@ export default class Main extends Component {
     state = {
         productInfo: {},
         docs: [],
-        page: 1
+        page: 1,
+        screenHeight: Dimensions.get('window').height,
+        screenWidth: Dimensions.get('window').width,
     };
 
     componentDidMount(){
@@ -39,75 +42,44 @@ export default class Main extends Component {
         this.loadProducts(pageNumber);
     }
 
+    scrollToTop = () => {
+        this.scroller.scrollTo({x: 0, y: 0});
+    };
+
     renderItem = ({ item }) => {
         return(
-           <View style={styles.productContainer}>
-               <Text style={styles.productTitle}>{item.title}</Text>
-               <Text style={styles.productDescription}>{item.description}</Text>
-               <TouchableOpacity
-               style={styles.productButton}
-               onPress={() => {this.props.navigation.navigate("Product", {product: item});}} >
-                <Text style={styles.productButtonText}>Acessar</Text>
-               </TouchableOpacity>
-           </View>
+            <View style={styles.productContainer}>
+                <Text style={styles.productTitle}>{item.title}</Text>
+                <Text style={styles.productDescription}>{item.description}</Text>
+                <TouchableOpacity
+                style={styles.productButton}
+                onPress={() => {this.props.navigation.navigate("Product", {product: item});}} >
+                    <Text style={styles.productButtonText}>Acessar</Text>
+                </TouchableOpacity>
+            </View>
         );
     }
 
     render() {
         return(
-            <View style={styles.container}>
-                <FlatList
-                contentContainerStyle = {styles.list}
-                data = {this.state.docs}
-                keyExtractor = {item => item._id}
-                renderItem = {this.renderItem}
-                onEndReached = {this.loadMore}
-                onEndReachedThreshold = {0.1} />
-            </View>
+            <ScrollView style={styles.container} ref={(scroller) => {this.scroller = scroller}}>
+                <View style={styles.container}>
+                    <FlatList
+                    contentContainerStyle = {styles.list}
+                    data = {this.state.docs}
+                    keyExtractor = {item => item._id}
+                    renderItem = {this.renderItem}
+                    onEndReached = {this.loadMore}
+                    onEndReachedThreshold = {0.1} />
+                </View>
+                <View>
+                    <TouchableOpacity onPress={this.scrollToTop}>
+                        <View>
+                            <Text>Scroll to Top</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: "#FAFAFA" 
-    },
-    list: {
-        padding: 20
-    },
-    productContainer: {
-        backgroundColor: '#FFF',
-        borderWidth: 1,
-        borderColor: '#DDD',
-        borderRadius: 5,
-        padding: 20,
-        marginBottom: 20
-    },
-    productTitle:{
-        fontSize: 18,
-        fontWeight: "bold",
-        color: "#333"
-    },
-    productDescription: {
-        fontSize: 16,
-        color: "#999",
-        marginTop: 5,
-        lineHeight: 24
-    },
-    productButton: {
-        height: 42,
-        borderRadius: 5,
-        borderWidth: 2,
-        borderColor: "#DA552F",
-        backgroundColor: "transparent",
-        justifyContent: "center",
-        alignItems: 'center',
-        marginTop: 10
-    },
-    productButtonText: {
-        fontSize: 16,
-        color: "#DA552f",
-        fontWeight: 'bold'
-    }
-});
