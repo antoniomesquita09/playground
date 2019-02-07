a70e2c87248226de893072f66dfc732da5cf4862
antoniomesquita09 <antoniomesquita09@gmail.com>
2019-02-07 14:16:25 -0200

    Fix: updating remote repository

---
+++ hunt/src/config/aws-exports.js
@@ -0,0 +1,8 @@
+const awsConfig = {
+    identityPoolId: 'us-east-1_Ab129faBb',
+    region: 'us-east-1',
+    userPoolId: 'us-east-1_R5MSimTcB',
+    userPoolWebClientId: '36u7imkvs9qpm8bsv0dvlvft84'
+  }
+  
+  export default awsConfig;
